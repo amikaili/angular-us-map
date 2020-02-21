1d749fdf7a43f425719259e86c5751753bafe76f
Arman <amikaili@uiowa.edu>
2020-02-21 17:26:33 -0600

    Pick selected states

--- custom-map-module/src/app/app.component.ts
+++ custom-map-module/src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   selectedState: any = [
-    {'code': 'ND', 'users': 324, 'org type' :'Service Provider'},
-    {'code': 'WA', 'users': 454, 'org type' :'Manufacturer'},
-    {'code':'AZ', 'users': 234, 'org type' :'Service Provider'},
-    {'code' : 'AK', 'users': 544, 'org type' :'Manufacturer'},
-    {'code' : 'CT', 'users': 544, 'org type' :'Manufacturer'},
-    {'code' : 'DC', 'users': 544, 'org type' :'Manufacturer'},
+    {'code': 'MD', 'users': 324, 'org type' :'Service Provider'},
+    {'code': 'NC', 'users': 454, 'org type' :'Manufacturer'},
+    // {'code':'AZ', 'users': 234, 'org type' :'Service Provider'},
+    // {'code' : 'AK', 'users': 544, 'org type' :'Manufacturer'},
+    // {'code' : 'CT', 'users': 544, 'org type' :'Manufacturer'},
+    // {'code' : 'DC', 'users': 544, 'org type' :'Manufacturer'},
   ];
 }
