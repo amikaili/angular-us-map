8ecf29d8b4bc601e8a65282037ac543928294c58
Arman <amikaili@uiowa.edu>
2020-05-19 13:36:42 -0500

    Added visualization libraries, Google Chart

--- custom-map-module/src/app/app.module.ts
+++ custom-map-module/src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+import { FlexmonsterPivotModule } from "ng-flexmonster";
+import { GoogleChartsModule } from "angular-google-charts";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import {SlMapModule } from './sl-map/components/sl-map.module';
 import { AppComponent } from './app.component'; 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , SlMapModule],
+  imports: [
+    BrowserModule,
+    FormsModule ,
+    FlexmonsterPivotModule,
+     SlMapModule,
+    GoogleChartsModule.forRoot()
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
